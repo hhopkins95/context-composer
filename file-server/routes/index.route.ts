1451b679ba99d5e2e7ee4e6a9b74ed08fd3295f1
Hunter Hopkins <hunterhopkins@mac.lan>
2024-12-09 15:57:18 -0800

    add basic file router

--- file-server/routes/index.route.ts
+++ file-server/routes/index.route.ts
@@ -1,29 +1,101 @@
 import * as HTTP_STATUS_CODES from "stoker/http-status-codes";
 import { jsonContent } from "stoker/openapi/helpers";
-import { createMessageObjectSchema } from "stoker/openapi/schemas";
 import { z } from "zod";
 import { createRouter } from "../lib/create-app.ts";
+import { readdir } from "node:fs/promises";
+import { join } from "node:path";
+import { readFile } from "node:fs/promises";
 
 const router = createRouter()
+  // Get Directory Tree
   .openapi({
-    tags: ["Index"], // Group within the OpenAPI doc
+    tags: ["Directory"], // Group within the OpenAPI doc
     method: "get",
-    path: "/",
+    path: "/directory",
     request: {
-      params: z.object({
-        dirPath: z.string().optional(),
+      query: z.object({
+        path: z.string(),
+        fileExtensions: z.string().array().default([
+          ".md",
+          ".txt",
+          ".json",
+          ".ts",
+          ".tsx",
+          ".js",
+          ".jsx",
+          ".css",
+          ".scss",
+          ".html",
+          ".htm",
+          ".xml",
+          ".yaml",
+          ".yml",
+          ".jsonc",
+          ".json5",
+        ]),
       }),
     },
     responses: {
-      // This is the same as above but using the stoker helpers
       [HTTP_STATUS_CODES.OK]: jsonContent(
-        createMessageObjectSchema("Hello from the index route"),
-        "Definition of this route...",
+        z.object({
+          files: z.string().array(), // array of all filepaths in this directory
+          dirs: z.string().array(), // array of all subdirectories in this directory
+        }),
+        "Description",
       ),
     },
-  }, (c) => {
+  }, async (c) => {
+    const { path: dirPath, fileExtensions } = c.req.valid("query");
+    console.log(dirPath, fileExtensions);
+
+    const res = await readdir(dirPath, {
+      withFileTypes: true,
+      recursive: false, // true,
+    });
+    const filePaths = res
+      .filter((file) => {
+        if (!file.isFile()) return false;
+        const ext = "." + file.name.split(".").pop();
+        return fileExtensions.includes(ext);
+      })
+      .map((file) => join(dirPath, file.name));
+
+    const subDirs = res.filter((file) => file.isDirectory()).map((file) =>
+      file.name
+    );
+
     return c.json({
-      message: "Hello from the index route",
+      files: filePaths,
+      dirs: subDirs,
+    });
+  })
+  // Read a file's contents as text
+  .openapi({
+    tags: ["Directory"],
+    method: "get",
+    path: "/file",
+    request: {
+      query: z.object({
+        path: z.string(),
+      }),
+    },
+    responses: {
+      [HTTP_STATUS_CODES.OK]: jsonContent(
+        z.object({
+          fileContent: z.string(),
+        }),
+        "File contents as text",
+      ),
+    },
+  }, async (c) => {
+    const { path } = c.req.valid("query");
+    console.log(path);
+
+    // Read the file content as UTF-8 text
+    const fileContent = await readFile(path, "utf-8");
+    console.log(fileContent);
+    return c.json({
+      fileContent,
     });
   });
 
